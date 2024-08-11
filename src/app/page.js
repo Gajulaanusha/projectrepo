515e0325ca8d66e8f4f27d8b4dd8b73e4777526e
Anusha <160407784+Gajulaanusha@users.noreply.github.com>
2024-08-11 09:26:22 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,12 @@
 "use client"
 import Image from "next/image";
 import styles from "./page.module.css";
-import {useState} from "react"
+
 export default function Home() {
-  const [cnt,setCnt]=useState(0)
-  const fn=()=>{
-    setCnt(cnt+1)
-  }
+  
+
   return <div>
-    <h1>{cnt}</h1>
-    <button onClick={fn()}>click</button>
+    <h1>cnt</h1>
+    <button>click</button>
   </div>
 }
